b24689633456460fd3fe5ee7ecde66db981ab107
OnlineReport.de <49470908+phpSoftware@users.noreply.github.com>
2020-04-09 10:59:06 +0200

    Update icon.php

--- icon.php
+++ icon.php
@@ -1,59 +1,59 @@
 <?php /* 60 Lines */
 
-// SETTINGS - give it a try and you will see ;-)
-$cssClassOrTagName = '.icon.'; // OR
-#$cssClassOrTagName = 'icon[';
-$cssClassOrTagEnd  = ''; // OR
-#$cssClassOrTagEnd  = ']';
+// SETTINGS
+$cssClassOrTagName = '.icon.';
+$cssClassOrTagEnd  = '';
 
-// START PRINTING CSS AS TEXT
-header('Content-Type: text/plain; charset=utf-8');
-// PRINT REMARKS AND BASIC CSS FOR ALL ICONS
-echo '/*
+
+// MAIN
+$css = '/*
 
 CSS Feather Icons
 =================
 
-> v4.24.0 (2019-08-08)
+> v2.0.0 (2020-04-09)
 
 __Download [Feather Icons](https://feathericons.com/feather.zip) 
 and run `icon.php` on e.g. localhost. 
 It will read SVG icons from the folder `feather` and build css from it.
-Save the CSS from the browser as a file and use it without the SVG Icons!__
+Now you can use the `feather.css` without the SVG Icons!__
 
 License
 -------
 
 Licensed under the MIT License.
 
-The icons that are used in this code are from feathericons.com.
+The icons that are used in this code are from feathericons.com
 
 They are also licensed under the MIT License.
 
-© 2019 phpSoftware https://github.com/phpSoftware/CSS-Feather-Icons
+© 2019 [phpSoftware](https://github.com/phpSoftware/CSS-Feather-Icons)
 
 */
 '.PHP_EOL.rtrim($cssClassOrTagName,'. [').' {
   display: inline-block;
-  height: 16px;
-  width: 16px;
+  height: 24px;
+  width: 24px;
   vertical-align: -0.125px;
   background-size: contain;'.PHP_EOL.'}'.PHP_EOL;
-// LOOP EACH ICON
+$html = '';
+$counter = 0;
 foreach (glob("feather/*.svg") as $file) {
-  // READ SVG ICON
+  ++$counter;
   $svg = file_get_contents($file);
-  // ENCODE SVG FOR CSS
+  $svg = str_replace ("\n", '', rtrim($svg));
+  $svg = str_replace (' />', '/>', $svg);
   $svg = str_replace(array('<','"','>'), array('%3C',"'",'%3E'), $svg);
-  // CLEAN ICON NAME FOR CSS
   $name = str_replace(array('feather/','.svg'), array('',''), $file);
-  // REMOVE CLASS ATTRIBUTE FROM SVG 
   $svg = str_replace(" class='feather feather-{$name}'", '', $svg);
-  // MAKE heart ICON FILLED AND RED - change hex e71837 for red if you like ;-)
-  if ($name == 'heart')
-    $svg = str_replace(
-      "fill='none' stroke='currentColor'", "fill='%23e71837' stroke='%23e71837'", $svg);
-  // PRINT EACH ICON CSS TO BROWSER
-  echo PHP_EOL.$cssClassOrTagName.$name.$cssClassOrTagEnd.' {
+  // MAKE heart ICON RED - change hex e71837 for red if you like ;-)
+  if ($name == 'heart') $svg = str_replace("stroke='currentColor'", "stroke='%23e71837'", $svg);
+  $css .= PHP_EOL.$cssClassOrTagName.$name.$cssClassOrTagEnd.' {
   background-image: url("data:image/svg+xml,'.$svg.'");'.PHP_EOL.'}'.PHP_EOL;
+  $html .= "<i class='icon {$name}'></i>";
 }
+file_put_contents('feather.css', $css);
+$header = '<!DOCTYPE HTML><html><head><meta charset="UTF-8"><title>Feather Icons CSS Test</title>'.
+          '<link href="feather.css" rel="stylesheet"></head><body><tt><h1>'.$counter.' Feather Icons CSS Test</h1>';
+file_put_contents('test.htm', $header.$html);
+echo '<tt><b>'.$counter.' Feather Icons CSS is ready, <a target="test" style="color:firebrick" href="test.htm">test it</a>!';
